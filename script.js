86e4e8fd1117807b1200e97ccffcffbfc0159c01
FredLavoie <flavoie3@gmail.com>
2021-06-11 16:15:43 -0400

    Add script to capture input characters dynamically and check against array

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+autocompleteSearchTerms = [
+	'about',
+	'above',
+	'across',
+	'app',
+	'apple',
+	'appreciate',
+	'bad',
+	'ball',
+	'balloon',
+	'bell',
+	'cat'
+]
+
 document.addEventListener('DOMContentLoaded', () => {
+	const searchTerm = document.querySelector('#search-term');
+	searchTerm.value = '';
+
+	searchTerm.addEventListener('input', (event) => {
+		if (searchTerm.value.length < 3) return;
+		for (const ea of autocompleteSearchTerms) {
+			if (ea.includes(searchTerm.value)) {
+				const item = document.createElement('div');
+				item.value = ea;
+				searchTerm.appendChild(item);
+				console.log('ea: ', ea);
+			}
+		}
+	});
 
 	const submit = document.querySelector('#search-form');
-	submit.addEventListener('submit', async (event) => {
+	submit.addEventListener('submit', (event) => {
 		event.preventDefault();
-		const searchTerm = document.querySelector('#search-term');
+		document.querySelector('#result-image').setAttribute('src', '');
 
-		fetch(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=DLCVuTK6KZExOS7JoMq82bi5MaI6EbWO&limit=1`, {
+		fetch(`https://api.giphy.com/v1/gifs/search?q=${searchTerm.value}&api_key=DLCVuTK6KZExOS7JoMq82bi5MaI6EbWO&limit=1`, {
 			method: 'GET',
 		})
 			.then((res) => res.json())
 			.then((data) => {
-				console.log('success');
+				document.querySelector('#result-image').setAttribute('src', data.data[0].images.original.url);
 			});
 
 	});
